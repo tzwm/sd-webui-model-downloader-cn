0d17f7ad38d4daa60faf1dcfeb5a64bd2374637d
tzwm <git@tzwm.me>
2023-09-10 16:30:10 +0800

    feat: move svelte to submodule

--- scripts/model-downloader-cn.py
+++ scripts/model-downloader-cn.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 import gradio as gr
 import html
 import os
+import time
 
 pre_path = "/model_downloader_cn"
 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -11,20 +12,20 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as view:
         with gr.Row():
-            path = f"{pre_path}/index.html"
+            path = f"{pre_path}/index.html?timestamp={int(time.time())}"
             gr.HTML(f"""
                 <iframe id="model_downloader_cn_frame" src="{html.escape(path)}"
-                  style="width: 100%; height: 100%;"></iframe>
+                  style="width: 100%; height: 640;"></iframe>
             """)
 
-    return [(view, "模型下载", "model_downloader_cn_tab")]
+    return [(view, "好捏模型", "model_downloader_cn_tab")]
 
 def on_app_started(_: gr.Blocks, app: FastAPI):
-    static_path = f"{current_path}/../svelte/build"
+    static_path = f"{current_path}/../good-to-nie/build"
     app.mount(
-            pre_path,
-            StaticFiles(directory=static_path),
-            name="model_downloader_cn-fe-static")
+        pre_path,
+        StaticFiles(directory=static_path),
+        name="model_downloader_cn-fe-static")
 
 script_callbacks.on_app_started(on_app_started)
 script_callbacks.on_ui_tabs(on_ui_tabs)
